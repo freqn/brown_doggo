762d561d009812976950c433b4dc7c3f800eb48a
freqn <ceyler@gmail.com>
2018-04-29 10:02:30 -0400

    update with proper module to test price fetching

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-import binance
-from setup import API_KEY, API_SECRET
-binance.set(API_KEY, API_SECRET)
+from binance.client import Client
+import pdb
 
+from token import API_KEY, API_SECRET
 
-print binance.prices()
+client = Client(API_KEY, API_SECRET)
+
+# get market depth
+depth = client.get_order_book(symbol='BNBBTC')
+
+# place a test market buy order, to place an actual order use the create_order function
+
+# get all symbol prices
+prices = client.get_all_tickers()
+
+for i in prices:
+    print(i)
