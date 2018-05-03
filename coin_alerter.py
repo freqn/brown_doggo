75b238198b5fde5018a72657fc6a7c536d976385
freqn <ceyler@gmail.com>
2018-05-03 15:25:42 -0400

    chore - refactor/objectify runner

--- coin_alerter.py
+++ coin_alerter.py
@@ -1,4 +1,3 @@
-# resources
 import pygame
 import pdb
 from time import sleep
@@ -7,48 +6,53 @@ from decimal import Decimal
 from binance.client import Client
 import os
 
-# config
-tracker = []
-client = Client(user.API_KEY, user.API_SECRET)
+class Runner:
+    def __init__(self):
+        self.tracker = []
+        self.moving_averages = []
+        self.client = Client(user.API_KEY, user.API_SECRET)
 
-# actions
-def get_market_data():
-    return client.get_all_tickers()
+    def run_job(self):
+        for i in range(0,20000):
+            self.data = self.client.get_all_tickers()
+            self.result = self.update_tracker(self.get_bitcoin_usd('BTCUSDT'))
+            if len(self.moving_averages) > 4:
+                self.moving_averages.pop(0)
+            self.append_result(str(self.result[0]))
+            if len(self.tracker) > 1 and self.tracker[0] > self.tracker[1]:
+                self.play_alert()
+                print self.result, "ALERT ALERT - SHUTDOWN RIG"
+            else:
+                print self.result, "OK"
+            print(self.moving_averages)
+            sleep(5)
 
-def get_bitcoin_usd(coin):
-    # pdb.set_trace()
-    for record in data:
-        if coin in record['symbol']:
-            usd_value = Decimal(record['price'])
-            return usd_value
+    def update_tracker(self, usd_value):
+        if len(self.tracker) < 2:
+            self.tracker.append(usd_value)
+        else:
+            self.tracker.pop(0)
+            self.tracker.append(usd_value)
+        return self.tracker
 
-def update_tracker(usd_value):
-    if len(tracker) < 2:
-        tracker.append(usd_value)
-    else:
-        tracker.pop(0)
-        tracker.append(usd_value)
-    return tracker
+    def get_bitcoin_usd(self, coin):
+        for record in self.data:
+            if coin in record['symbol']:
+                usd_value = Decimal(record['price'])
+                return usd_value
 
-def play_alert():
-    pygame.init()
-    pygame.mixer.music.load("music.mp3")
-    pygame.mixer.music.play()
+    def play_alert(self):
+        pygame.init()
+        pygame.mixer.music.load("music.mp3")
+        pygame.mixer.music.play()
 
-# pdb.set_trace()
-for i in range(0,20000):
-    data = client.get_all_tickers()
-    result = update_tracker(get_bitcoin_usd('BTCUSDT'))
-    if len(tracker) > 1 and tracker[0] > tracker[1]:
-        play_alert()
-        # os.system("say 'DANGER DANGER STRANGER. HOLD ONTO YOU BALLS!!!'")
-        print result, "ALERT ALERT - SHUTDOWN RIG"
-    else:
-        print result, "OK"
-    sleep(30)
+    def append_result(self, result):
+        self.moving_averages.append(result)
 
+Runner().run_job()
+# in progress:
+# - SMA with custom intervals
 
 # todos:
-# - Moving average with custom intervals
 # - Plotly
 # - Test trades
